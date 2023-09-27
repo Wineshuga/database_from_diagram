161bb1e4dfb0c69c406862368d5deb41e5d67797
Wineshuga <uzochukwuwinnie@gmail.com>
2023-09-27 14:39:10 +0100

    Create index for treatment id

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -14,45 +14,37 @@ CREATE TABLE
         date_of_birth DATE
     )
 
-CREATE TABLE treatments (
-  id INT GENERATED ALWAYS AS IDENTITY,
-  type VARCHAR(255),
-  name VARCHAR(255),
-  PRIMARY KEY (id)
-)
+CREATE TABLE
+    treatments (
+        id INT GENERATED ALWAYS AS IDENTITY,
+        type VARCHAR(255),
+        name VARCHAR(255),
+        PRIMARY KEY (id)
+    )
 
+CREATE TABLE
+    invoice_items (
+        id INT GENERATED ALWAYS AS IDENTITY,
+        unit_price decimal(8, 2),
+        quantity INT,
+        total_price decimal(8, 2),
+        invoice_id INT,
+        treatment_id INT,
+        name VARCHAR(255),
+        PRIMARY KEY (id) CONSTRAINT fk_invoice FOREIGN KEY (invoice_id) REFERENCES invoices(id) CONSTRAINT fk_treatment FOREIGN KEY (treatment_id) REFERENCES treatments(id)
+    )
 
-CREATE TABLE invoice_items (
-  id INT GENERATED ALWAYS AS IDENTITY,
-  unit_price decimal(8, 2),
-  quantity INT,
-  total_price decimal(8, 2),
-  invoice_id INT,
-  treatment_id INT,
-  name VARCHAR(255),
-  PRIMARY KEY (id)
-  CONSTRAINT 
-  fk_invoice
-  FOREIGN KEY (invoice_id)
-  REFERENCES invoices(id)
-  CONSTRAINT 
-  fk_treatment
-  FOREIGN KEY (treatment_id)
-  REFERENCES treatments(id)
-)
-
-CREATE TABLE invoices (
-  id INT GENERATED ALWAYS AS IDENTITY,
-  total_amount decimal(8, 2),
-  generated_at TIMESTAMP,
-  payed_at TIMESTAMP,
-  medical_history_id INT
-  PRIMARY KEY (id)
-  CONSTRAINT
-  fk_medical_history
-  REFERENCES medical_histories(id)
-)
-
+CREATE TABLE
+    invoices (
+        id INT GENERATED ALWAYS AS IDENTITY,
+        total_amount decimal(8, 2),
+        generated_at TIMESTAMP,
+        payed_at TIMESTAMP,
+        medical_history_id INT PRIMARY KEY (id) CONSTRAINT fk_medical_history REFERENCES medical_histories(id)
+    )
 
 CREATE INDEX patient_id_idx ON medical_histories(patient_id ASC);
+
 CREATE INDEX invoice_id_idx ON invoice_items(invoice_id ASC);
+
+CREATE INDEX treatment_id_idx ON invoice_items(treatment_id ASC);
