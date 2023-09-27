c6f6af588cb77d3804e16984f36f76ca6e2c4c63
akorede <emiolasaheed1@gmail.com>
2023-09-27 13:32:38 +0100

    create treatments table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -8,8 +8,15 @@ CREATE TABLE
     )
 
 CREATE TABLE
-    Patients (
+    patients (
         id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         name VARCHAR(255),
         date_of_birth DATE
     )
+
+CREATE TABLE treatments (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  type VARCHAR(255),
+  name VARCHAR(255),
+  PRIMARY KEY (id)
+)
